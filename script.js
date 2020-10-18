b9fe1182712918b179c4b8d4083667c4eea1362d
Amelia Goodson <ameliajanegoodson@gmail.com>
2020-10-18 22:48:07 +1100

    initial commit with working app

---
+++ script.js
@@ -0,0 +1,54 @@
+var inputPrice = document.getElementById("inputPrice")
+var inputPercent = document.getElementById("inputPercent")
+var inputPeople = document.getElementById("inputPeople")
+var tipAmountTotal = document.getElementById("tip-amount")
+var newTotal = document.getElementById("new-total")
+var totalEach = document.getElementById("total-each")
+var button = document.getElementById("submit")
+var clear = document.getElementById("clear")
+
+button.addEventListener("click", function (event) {
+    event.preventDefault()
+    calculateTip()
+})
+
+button.addEventListener("click", function () {
+    clear()
+})
+
+
+function calculateTip() {
+    var price = parseFloat(inputPrice.value)
+    var percent = parseFloat(inputPercent.value)
+    var people = parseFloat(inputPeople.value)
+
+    if (!percent || !price) {
+        alert("Please enter a price and percentage")
+        clear()
+    }
+    else {
+    var tip = tipAmountTotal.textContent = price * `0.${percent}`
+    var total = parseFloat(price + tip)
+    newTotal.textContent = total.toFixed(2)
+        // if no number of people is entered, assume one person
+    if (percent && price && !people) {
+        people = 1
+        totalEach.textContent = total
+    }
+    else {
+    var splitCalc = total / people
+    totalEach.textContent = splitCalc.toFixed(2)
+    }
+
+    }
+    
+}
+// Clear all fields and outputs
+function clear() {
+    inputPeople.value = ""
+    inputPercent.value = ""
+    inputPrice.value = ""
+    totalEach.textContent = ""
+    newTotal.textContent = ""
+    tip.textContent = ""
+}
